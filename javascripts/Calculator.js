f3dcbb62beb5de0341cf4aee8bbb0387da9da17f
jtpicazio <jtpicazio@gmail.com>
2018-09-03 23:10:01 -0400

    Calculator now checks for numbers with isNaN()

--- javascripts/Calculator.js
+++ javascripts/Calculator.js
@@ -2,23 +2,40 @@ function Addition() {
 	var x, y;
 	x = parseFloat(document.getElementById("firstNumber").value);
 	y = parseFloat(document.getElementById("secondNumber").value);
-	document.getElementById("answer").innerHTML = x + y;
+	if(isNaN(x) || isNaN(y)){
+		document.getElementById("answer").innerHTML = "Invalid Number";
+	}else{
+		document.getElementById("answer").innerHTML = x + y;
+	}
+	
 }
 function Subtraction() {
 	var x, y;
 	x = parseFloat(document.getElementById("firstNumber").value);
 	y = parseFloat(document.getElementById("secondNumber").value);
-	document.getElementById("answer").innerHTML = x - y;
+	if(isNaN(x) || isNaN(y)){
+		document.getElementById("answer").innerHTML = "Invalid Number";
+	}else{
+		document.getElementById("answer").innerHTML = x - y;
+	}
 }
 function Multiplication() {
 	var x, y;
 	x = parseFloat(document.getElementById("firstNumber").value);
 	y = parseFloat(document.getElementById("secondNumber").value);
-	document.getElementById("answer").innerHTML = x * y;
+	if(isNaN(x) || isNaN(y)){
+		document.getElementById("answer").innerHTML = "Invalid Number";
+	}else{
+		document.getElementById("answer").innerHTML = x * y;
+	}
 }
 function Division() {
 	var x, y;
 	x = parseFloat(document.getElementById("firstNumber").value);
 	y = parseFloat(document.getElementById("secondNumber").value);
-	document.getElementById("answer").innerHTML = x / y;
+	if(isNaN(x) || isNaN(y)){
+		document.getElementById("answer").innerHTML = "Invalid Number";
+	}else{
+		document.getElementById("answer").innerHTML = x / y;
+	}
 }
